5dd2f9053b98c9a3aebb83e7de50c03bab864a62
IvnKor <52051399+IvnKor@users.noreply.github.com>
2019-07-20 03:52:14 +0300

    Update install_ruby.sh

--- config-scripts/install_ruby.sh
+++ config-scripts/install_ruby.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-sudo apt update
-sudo apt install -y ruby-full ruby-bundler build-essential
+apt update
+apt install -y ruby-full ruby-bundler build-essential
 
-COMMAND='ruby -v'
-if $COMMAND | grep -q 'command not found'; then
-	echo "error: ruby  installation failuire"
+ruby -v
+echo $?
+if [ $? -eq 0 ]; then
+        echo "ruby  installation successfull"
 else
-	echo "error: ruby  installation successfull"
-fi
+        echo "error: ruby  installation failed" >&2
 
-COMMAND='bundler -v'
-if $COMMAND | grep -q 'command not found'; then
-	echo "error: bundler installation failuire"
-else
-	echo "error: bundler installation successfull"
 fi
